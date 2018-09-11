bfc62284430d63d8f130dc5f01c77447ca7fcc8e
Matt Smith and Scott Fraser <matt+scott@>
2018-09-11 16:39:27 -0700

    edit styles add postform

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import AppBar from './AppBar'
 import './App.css';
-import Feed from './Feed'
+import Feed from './Feed';
+import PostForm from './PostForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppBar />
+        <PostForm />
         <Feed />
       </div>
     );
